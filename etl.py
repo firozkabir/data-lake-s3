8741152ed922315f2320a9ddc002cde47e301290
Firoz Kabir <firozkabir@gmail.com>
2021-01-17 11:30:16 -0500

    added user defined schema for song_data and log_data

--- etl.py
+++ etl.py
@@ -7,6 +7,7 @@ import configparser
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, LongType
 from pyspark.sql.window import Window
 
 
@@ -66,6 +67,10 @@ def create_spark_session():
     return spark
 
 
+class IngeterType(object):
+    pass
+
+
 def process_song_data(spark, input_data, output_data):
     """
     Reads song_data from input_data S3 bucket,
@@ -76,9 +81,24 @@ def process_song_data(spark, input_data, output_data):
     :param output_data: an S3 bucket to save output data to
     :return:
     """
+    
+    song_schema = StructType([
+        StructField("num_songs", IntegerType(), True),
+        StructField("artist_id", StringType(), True),
+        StructField("artist_latitude", DoubleType(), True),
+        StructField("artist_longitude", DoubleType(), True),
+        StructField("artist_location", StringType(), True),
+        StructField("artist_name", StringType(), True),
+        StructField("song_id", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("duration", DoubleType(), True),
+        StructField("year", IntegerType(), True),
+
+    ])
+
     song_data = input_data + "/song_data/*/*/*"
     print(f"{datetime.now()} - reading song_data from {song_data}")
-    df = spark.read.json(song_data)
+    df = spark.read.json(song_data, song_schema)
     print(f"{datetime.now()} - done reading song_data.")
 
     print(f" {datetime.now()} - extracting columns song_id, title, artist_id, \
@@ -122,9 +142,32 @@ def process_log_data(spark, input_data, output_data):
     :return:
     """
 
+    log_schema = StructType([
+        StructField("artist", StringType(), True),
+        StructField("auth", StringType(), True),
+        StructField("firstName", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("itemInSession", IntegerType(), True),
+        StructField("lastName", StringType(), True),
+        StructField("length", DoubleType(), True),
+        StructField("level", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("method", StringType(), True),
+        StructField("page", StringType(), True),
+        StructField("registration", DoubleType(), True),
+        StructField("sessionId", IntegerType(), True),
+        StructField("song", StringType(), True),
+        StructField("status", IntegerType(), True),
+        StructField("ts", LongType(), True),
+        StructField("userAgent", StringType(), True),
+        StructField("userId", StringType(), True),
+
+
+    ])
+
     log_data = input_data + "/log_data/*/*"
     print(f"{datetime.now()} - reading log_data from {log_data}")
-    df = spark.read.json(log_data)
+    df = spark.read.json(log_data, log_schema)
     print(f"{datetime.now()} - done reading log_data.")
 
     print(f"{datetime.now()} - extracting columns user_id, first_name, last_name, gender, level \
@@ -170,9 +213,23 @@ def process_log_data(spark, input_data, output_data):
     print(f"{datetime.now()} - filtering dataframe for page='NextSong' before creating the fact table songplays ")
     df = df.where(df.page == 'NextSong')
 
+    song_schema = StructType([
+        StructField("num_songs", IntegerType(), True),
+        StructField("artist_id", StringType(), True),
+        StructField("artist_latitude", DoubleType(), True),
+        StructField("artist_longitude", DoubleType(), True),
+        StructField("artist_location", StringType(), True),
+        StructField("artist_name", StringType(), True),
+        StructField("song_id", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("duration", DoubleType(), True),
+        StructField("year", IntegerType(), True),
+
+    ])
+
     song_data = input_data + "/song_data/*/*/*"
     print(f"{datetime.now()} - read in song_data from {song_data} to use for fact table songplays")
-    song_df = spark.read.json(song_data)
+    song_df = spark.read.json(song_data, song_schema)
     print(f"{datetime.now()} - done loading song_data.")
 
     print(f"{datetime.now()} - extracting columns from joined song and log datasets \
