b2ca754482bcca3bc486dac68ee8f18303034be9
Firoz Kabir <firozkabir@gmail.com>
2021-01-17 08:49:22 -0500

    done.

---
+++ README.md
@@ -0,0 +1,193 @@
+# Cloud Data Lake with S3 and Spark
+### A project submitted as work for Udacity's Data Engineering Nano Degree 
+
+## Introduction 
+Sparkify is a startup providing music streaming service. Their application stores data in JSON format. 
+
+We want to use S3 on AWS as a datalake to store both raw data as well as the fact, dimension tables in parquet format. 
+We are using pyspark as our ETL pipeline to store the fact and dimension tables in parquet format. 
+From there, our analysts can use any analytical tool capable of reading parquest files to perform analysis 
+of user trends, listening trends etc.  
+
+
+## Summary
+We are building a data lake on AWS's S3 platform. 
+We load "song data" and "log data" (JSON files) from S3 bucket into spark dataframes.  
+Once data is loaded, we can run "ETL" which is a process of transforming and loading data in fact and dimension tables via pyspark. 
+Finally, we save the fact and dimensions as parquet files on S3. 
+
+All steps are executed via `etl.py` . Be sure to see detailed instructions below on how to setup this pyspark program.   
+
+
+### Staging dataframes - song_data, log_data:
+* staging dataframe song_data follows the structure of the song data: 
+    ```
+    {
+        "num_songs": 1, 
+        "artist_id": "ARJIE2Y1187B994AB7", 
+        "artist_latitude": null, 
+        "artist_longitude": null, 
+        "artist_location": "", 
+        "artist_name": "Line Renaud", 
+        "song_id": "SOUPIRU12A6D4FA1E1", 
+        "title": "Der Kleine Dompfaff", 
+        "duration": 152.92036, 
+        "year": 0
+    }
+    ```
+* staging dataframe log_data follow the structure of the log data:
+    ```
+    {
+        "artist":"Des'ree",
+        "auth":"Logged In",
+        "firstName":"Kaylee",
+        "gender":"F",
+        "itemInSession":1,
+        "lastName":"Summers",
+        "length":246.30812,
+        "level":"free",
+        "location":"Phoenix-Mesa-Scottsdale, AZ",
+        "method":"PUT",
+        "page":"NextSong",
+        "registration":1540344794796.0,
+        "sessionId":139,
+        "song":"You Gotta Be",
+        "status":200,
+        "ts":1541106106796,
+        "userAgent":"\"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/35.0.1916.153 Safari\/537.36\"",
+        "userId":"8"
+    }
+    ```
+
+### Fact table:
+* Fact table `songplays` describes the facts about each "songplay" event and has the following schema. 
+It can be used to analyze the listening trends of users, memebership upgrade patters as well as artist trends  
+```
+ |-- start_time: timestamp (nullable = true)
+ |-- user_id: string (nullable = true)
+ |-- level: string (nullable = true)
+ |-- song_id: string (nullable = true)
+ |-- artist_id: string (nullable = true)
+ |-- session_id: long (nullable = true)
+ |-- location: string (nullable = true)
+ |-- user_agent: string (nullable = true)
+ |-- year: integer (nullable = true)
+ |-- month: integer (nullable = true)
+ |-- songplay_id: integer (nullable = true)
+```
+
+
+### Dimenstion tables: 
+* Dimension table `time` is your classic time dimension translating timestamp into day, month, year, week etc.
+The table schema was prescribed in this project's specification and follows what was done in a redshift data warehouse.
+```
+ |-- start_time: timestamp (nullable = true)
+ |-- hour: integer (nullable = true)
+ |-- day: integer (nullable = true)
+ |-- week: integer (nullable = true)
+ |-- month: integer (nullable = true)
+ |-- year: integer (nullable = true)
+ |-- weekday: integer (nullable = true)
+```
+
+
+* Dimension table `users` describes each user and their leve in sparkify service. 
+The table schema was prescribed in this project's specification and follows what was done in a redshift data warehouse.
+```
+ |-- user_id: string (nullable = true)
+ |-- first_name: string (nullable = true)
+ |-- last_name: string (nullable = true)
+ |-- gender: string (nullable = true)
+ |-- level: string (nullable = true)
+```
+
+
+* Dimension table `songs` describes each song. Joining songs and artists tables to fact table will allow use to analyze 
+trends. The table schema was prescribed in this project's specification and follows what was done in a redshift data warehouse.
+```
+ |-- song_id: string (nullable = true)
+ |-- title: string (nullable = true)
+ |-- artist_id: string (nullable = true)
+ |-- year: long (nullable = true)
+ |-- duration: double (nullable = true)
+```
+
+
+* Dimension table `artists` describes each artist. 
+The table schema was prescribed in this project's specification and follows what was done in a redshift data warehouse.
+```
+ |-- artist_id: string (nullable = true)
+ |-- name: string (nullable = true)
+ |-- location: string (nullable = true)
+ |-- latitude: double (nullable = true)
+ |-- longtitude: double (nullable = true)
+```
+
+
+## Installing and running this application
+
+
+* Checkout code
+```
+git clone git@github.com:firozkabir/data-lake-s3.git
+```
+
+
+* Update configuration
+```
+[aws-keys]
+aws_access_key_id="AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+[s3-buckets]
+input_data="s3://udacity-dend/"
+output_data="s3://some-bucket/"
+```
+
+
+* Install dependencies
+```
+cd data-lake-s3
+virtualenv -p /usr/bin/python3 venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+
+* Run the application interactively 
+```
+./etl.py
+```
+
+
+## Prepare Local Development Environment 
+
+* Create virtual environment 
+
+```bash
+virtualenv -p /usr/bin/python3 venv
+```
+
+* Install dependencies 
+```bash
+source venv/bin/activate
+pip3 install pyspark
+```
+
+
+* Update `dl.cfg`, you can use local file system for `input_data` and `output_data` for development 
+```
+[aws-keys]
+aws_access_key_id="AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+[s3-buckets]
+input_data="s3://udacity-dend/"
+output_data="s3://some-bucket/"
+```
+
+* Run application locally
+```
+source venv/bin/activate
+./etl.py
+```
---
+++ etl.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from datetime import datetime
+import configparser
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+from pyspark.sql.window import Window
+
+
+def get_config(config_file='dl.cfg'):
+    """
+    gets aws and s3 configurations from dl.cfg file
+    shows the configuration on stdout for debugging
+    :param config_file:
+    :return:
+    """
+    try:
+
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        aws_id = config['aws-keys']['aws_access_key_id'].strip('"').strip("'")
+        aws_secret = config['aws-keys']['aws_secret_access_key'].strip('"').strip("'")
+        input_data = config['s3-buckets']['input_data'].strip('"').strip("'")
+        output_data = config['s3-buckets']['output_data'].strip('"').strip("'")
+
+    except Exception as e:
+        print(f"{datetime.now()} - error reading configs from {config_file}:")
+        print(f"{e}")
+        raise e
+
+    print(f"\n{'-' * 15} configs found {'-' * 15}")
+    print(f"aws_id: {aws_id}")
+    print(f"aws_secret: {aws_secret[:4]}{'*' * (len(aws_secret) - 4)}")
+    print(f"input_data: {input_data}")
+    print(f"output_data: {output_data}")
+    print(f"{'-' * 44}\n")
+
+    os.environ['AWS_ACCESS_KEY_ID'] = aws_id
+    os.environ['AWS_SECRET_ACCESS_KEY'] = aws_secret
+
+    return input_data, output_data
+
+
+def create_spark_session():
+    """
+    creates a local spark session, adds aws-hadoop package to the session
+    :return: SparkSession object
+    """
+    try:
+        spark = SparkSession \
+            .builder \
+            .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+            .getOrCreate()
+
+    except Exception as e:
+        print(f"{datetime.now()} - error creating spark session:")
+        print(f"{e}")
+        raise e
+
+    print(f"{datetime.now()} - spark session created")
+
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    """
+    Reads song_data from input_data S3 bucket,
+    performs data wrangling,
+    saves dimension tables songs and artists in parquet format in output_data S3 bucket
+    :param spark: a SparkSession object
+    :param input_data: an S3 bucket to get input data from
+    :param output_data: an S3 bucket to save output data to
+    :return:
+    """
+    song_data = input_data + "/song_data/*/*/*"
+    print(f"{datetime.now()} - reading song_data from {song_data}")
+    df = spark.read.json(song_data)
+    print(f"{datetime.now()} - done reading song_data.")
+
+    print(f" {datetime.now()} - extracting columns song_id, title, artist_id, \
+                                year and duration to create dimension table 'songs'")
+    songs_table = df.select("song_id", "title", "artist_id", "year", "duration")
+
+    print(f"{datetime.now()} - writing dimension table 'songs' to parquet file \
+                            {output_data + '/songs.parquet'} partitioned by year and artist_id")
+    songs_table.write \
+        .option("compression", "gzip") \
+        .mode('overwrite') \
+        .partitionBy("year", "artist_id").parquet(output_data + "/songs.parquet")
+
+    print(f"{datetime.now()} - extracting columns artist_id, name, location, lattitude and longitude \
+                                to create dimension table artists")
+    artists_table = df.selectExpr("artist_id",
+                                  "artist_name as name",
+                                  "artist_location as location",
+                                  "artist_latitude as latitude",
+                                  "artist_longitude as longtitude")
+
+    print(f"{datetime.now()} - writing dimension table artists to parquet file {output_data + '/artists.parquet'}")
+    artists_table.write \
+        .option("compression", "gzip") \
+        .mode("overwrite") \
+        .parquet(output_data + '/artists.parquet')
+
+    print(f"{datetime.now()} - done processing song_data")
+
+
+def process_log_data(spark, input_data, output_data):
+    """
+    Reads both song_data and log_data.
+    Performs data wrangling.
+    Creates fact table songplays.
+    Creates dimension tables time, users.
+    Saves each table in output_data S3 bucket in parquet format.
+    :param spark: a SparkSession object
+    :param input_data:
+    :param output_data:
+    :return:
+    """
+
+    log_data = input_data + "/log_data/*/*"
+    print(f"{datetime.now()} - reading log_data from {log_data}")
+    df = spark.read.json(log_data)
+    print(f"{datetime.now()} - done reading log_data.")
+
+    print(f"{datetime.now()} - extracting columns user_id, first_name, last_name, gender, level \
+                            to create dimension table users")
+    users_table = df.where(df.userId.isNotNull()) \
+        .selectExpr("userId as user_id",
+                    "firstName as first_name",
+                    "lastName as last_name",
+                    "gender",
+                    "level") \
+        .distinct()
+
+    print(f"{datetime.now()} - writing dimension table users to parquet files at {output_data + '/users.parquet'}")
+    users_table.write \
+        .option("compression", "gzip") \
+        .mode("overwrite") \
+        .parquet(output_data + '/users.parquet')
+
+    get_datetime_udf = f.udf(lambda x: datetime.fromtimestamp(x / 1000).strftime('%Y-%m-%d %H:%M:%S'))
+
+    print(f"{datetime.now()} - extracting columns start_time, hour, day, week, month, year, weekday \
+                            to create dimension table time")
+
+    df = df.withColumn('start_time', get_datetime_udf(df.ts).cast("timestamp"))
+    time_table = df.where(df.start_time.isNotNull()) \
+        .select("start_time") \
+        .distinct()
+
+    time_table = time_table.withColumn("hour", f.hour(f.col("start_time"))) \
+        .withColumn("day", f.dayofmonth(f.col("start_time"))) \
+        .withColumn("week", f.weekofyear(f.col("start_time"))) \
+        .withColumn("month", f.month(f.col("start_time"))) \
+        .withColumn("year", f.year(f.col("start_time"))) \
+        .withColumn("weekday", f.dayofweek(f.col("start_time")))
+
+    print(f"{datetime.now()} - writing dimension table time to parquet files at {output_data + '/time.parquet'}")
+    time_table.write \
+        .option("compression", "gzip") \
+        .mode("overwrite") \
+        .parquet(output_data + '/time.parquet')
+
+    print(f"{datetime.now()} - filtering dataframe for page='NextSong' before creating the fact table songplays ")
+    df = df.where(df.page == 'NextSong')
+
+    song_data = input_data + "/song_data/*/*/*"
+    print(f"{datetime.now()} - read in song_data from {song_data} to use for fact table songplays")
+    song_df = spark.read.json(song_data)
+    print(f"{datetime.now()} - done loading song_data.")
+
+    print(f"{datetime.now()} - extracting columns from joined song and log datasets \
+                                to create songplays table")
+
+    songplays_table = df.join(song_df,
+                              [df.song == song_df.title,
+                               df.artist == song_df.artist_name],
+                              how='inner') \
+        .selectExpr("start_time",
+                    "userId as user_id",
+                    "level",
+                    "song_id",
+                    "artist_id",
+                    "sessionId as session_id",
+                    "location",
+                    "userAgent as user_agent"
+                    )
+
+    songplays_table = songplays_table.withColumn("year", f.year(f.col("start_time"))) \
+        .withColumn("month", f.month(f.col("start_time"))) \
+        .withColumn("songplay_id", f.row_number().over(Window.orderBy("session_id")))
+
+    print(f"{datetime.now} - write songplays table to parquet files partitioned by year and month")
+    songplays_table.write \
+        .option("compression", "gzip") \
+        .mode("overwrite") \
+        .partitionBy(["year", "month"]) \
+        .parquet(output_data + "/songplays.parquet")
+
+    print(f"{datetime.now()} - done processing log_data")
+
+
+def main(argv):
+    print(f"*** {datetime.now()} - {argv[0]} - start ***")
+
+    config_file = "dl.cfg"
+
+    print(f"\n{datetime.now()} - getting config from {config_file}")
+    input_data, output_data = get_config()
+
+    print(f"\n{datetime.now()} - setting up spark session ")
+    spark = create_spark_session()
+
+    print(f"\n{datetime.now()} - processing song_data ")
+    process_song_data(spark, input_data, output_data)
+
+    print(f"\n{datetime.now()} - processing log_data ")
+    process_log_data(spark, input_data, output_data)
+
+    print(f"=== {datetime.now()} - {argv[0]} - end ===")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
